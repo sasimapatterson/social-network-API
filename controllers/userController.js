2ed9d06a2ef38c5a1f7aaf37c921b2797717c0ae
Sasi Patterson <sweetpea_th@hotmail.com>
2022-10-13 17:31:07 -0400

    found the bug params.friendId instead of req.params.friendId, fixed.

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,7 +86,7 @@ module.exports = {
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friends: params.friendId }},
+      { $pull: { friends: req.params.friendId }},
       { runValidators: true, new: true }
     )
     .then((user) =>
