c3270e508611d493cc43cf82178e937953fc7740
Sasi Patterson <sweetpea_th@hotmail.com>
2022-10-12 23:51:57 -0400

    created userController.js but couldn't test it yet, still need to get the thougthController.js done before I can run the server.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,14 @@
 const { User } = require('../models');
 
 module.exports = {
-  // get all users
+  // GET all users
   getAllUser(req, res) {
     User.find({})
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
 
-  //  get one user using id
+  //  GET one user using id
   getUserById(req, res) {
     User.findOne({ _id: req.params.userId })
       // to remove the weird symbol
@@ -26,9 +26,75 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
+  // POST create a user
   createUser(req, res) {
     User.create(req.body)
-      .then((dbUserData) => res.json(dbUserData))
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+
+  // PUT update user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this ID' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // DELETE user
+  deleteUser(req, res) {
+    User.findOneAndDelete(
+      { _id: req.params.userId }
+    )
+    .then((user) => 
+      !user
+        ? res.status(404).json({ message: 'No user with this ID' }) 
+        : res.json(user)
+      )
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
+
+  // POST add friend to user's friend list. Use activity 25 for reference
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId }},
+      { runValidators: true, new: true }
+    )
+    .then((user) => 
+      !user
+        ? res.status(404).json({ message: 'No user with this ID' })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+
+  // DELETE remove friend
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: params.friendId }},
+      { runValidators: true, new: true }
+    )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: 'No user with this ID' })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
 };
+
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const { User } = require('.');
+
 
 // Schema to create User model
 const userSchema = new Schema(
